ffbcecb9a701a1ada4c4aa69291b181e3c607c19
elhajuojy <souirimehdi311@gmail.com>
2023-01-16 14:14:33 +0100

    add class based files

--- website/public/index.php
+++ website/public/index.php
@@ -1,8 +1,18 @@
 <?php
 
 
+
 session_start();
+
 const BASE_PATH = __DIR__ . '/../';
+
+require_once BASE_PATH . 'vendor/autoload.php';
+$envPath = './';
+
+
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(BASE_PATH);
+$dotenv->safeload();
+
 
 
 require BASE_PATH . "View/Home.php";
