580da9c51983f58b1a2d0d962e9beb11a4c9bf79
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2023-01-18 15:28:41 +0100

    config the project

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const color = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin  = require('tailwindcss/plugin');
+
+
 module.exports = {
-  content: ["./**/*.{html,js,php,jsx,ts,tsx}"],
+  //dark mode is set to class so that we can use it in the html not to a local machine setting
+  darkMode : "class",
+  content: ["./**/*.{html,js,ts,jsx,tsx,php}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-1' : "#F5F7FF",
+        'color-2' : "#0074BE",
+        'color-3' : "#0156FF",
+        'color-4' : "#01A4FF",
+        'color-5' : "#A2A6B0",
+        'color-6' : "#CACDD8",
+        'color-7' : "#000000",
+        'color-8' : "#C94D3F",
+        'color-9' : "#78A962",
+        'color-10' : "#666666",
+      },
+      fontFamily: {
+        'poppins': ['"Poppins"', 'sans-serif'],
+      },
+      borderRadius:  {
+        'small': '0.25rem',
+      }
+    },
+
   },
   plugins: [],
 }
