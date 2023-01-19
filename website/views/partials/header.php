2778c955a6a6101104b8607b1383de651ac8f0ff
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2023-01-19 09:41:01 +0100

    add 404 adn 403 pages

--- website/views/partials/header.php
+++ website/views/partials/header.php
@@ -20,6 +20,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet">
-    <?= $cssLinks ?> 
+    <?= $cssLinks??'' ?> 
 </head>
 <body class=" scroll-smooth  ease-in duration-500  ">
