1d54405ee6c18fe680c2385bc5c525714c234c2a
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2023-01-19 17:36:13 +0100

    update route

--- website/routes.php
+++ website/routes.php
@@ -2,7 +2,7 @@
 
 
 $router->get('/', 'Controllers\HomeController.php');
-$router->get('/product', 'Controllers\ProductsController.php');
+// $router->get('/product', 'Controllers\ProductsController.php');
 
 
 $router->get('/admin/login', 'Controllers\Admin\Login.php');
@@ -14,10 +14,9 @@ $router->get('/admin/logout', 'Controllers\Admin\Logout.php');
 $router->get('/admin/dashboard', 'Controllers\Admin\Dashboard.php');
 
 $router->get('/admin/dashboard/products', 'Controllers\Admin\Dashboard.php');
-$router->get('/admin/dashboard/products/create', 'Controllers\Admin\Dashboard.php');
-$router->post('/admin/dashboard/products/create', 'Controllers\Admin\Dashboard.php');
-$router->get('/admin/dashboard/products/edit', 'Controllers\Admin\Dashboard.php');
-$router->post('/admin/dashboard/products/edit', 'Controllers\Admin\Dashboard.php');
-$router->delete('/admin/dashboard/products', 'Controllers\Admin\Dashboard.php');
-$router->delete('/admin/dashboard/', 'Controllers\Admin\Dashboard.php');
+
+$router->get('/product', 'Controllers\Admin\product\show.php');
+$router->post('/product', 'Controllers\Admin\product\create.php');
+$router->delete('/product', 'Controllers\Admin\product\destory.php');
+
 
