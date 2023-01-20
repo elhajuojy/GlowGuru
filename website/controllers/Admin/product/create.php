2a72655cd04718e37b1ff8c18f4dfc269f79968c
ELMAHDI ELHJUOJY <86893073+elhajuojy@users.noreply.github.com>
2023-01-20 19:21:45 +0100

    create products

--- website/controllers/Admin/product/create.php
+++ website/controllers/Admin/product/create.php
@@ -1,60 +1,47 @@
-<?php 
 
-
-
-// echo 'Create Product';
-// dd($_POST);
+<?php
 
 use core\App;
 use core\Database;
 
-$data = $_POST['data'];
-$form = $_POST['forms'];
+// if(!isAdmin()){
+//     goToPage('/login');
+// }
+
+
 $db = App::resolve(Database::class);
-json_decode($data);
 
+// dd($_POST['product_image]);
+//get info 
 
-// dd($data[0]);
-
-$images = $_FILES['product_images'];
-$imagesdir =  [];
-
-foreach ($images['name'] as $key => $value) {
-    if ($images['error'][$key] == 0) {
-        $tmp_name = $images['tmp_name'][$key];
-        $name = $images['name'][$key];
-        array_push($imagesdir, $name);
-        move_uploaded_file($tmp_name, "assets/images/products/$name");
-    }
+$imagename = "";
+$image_dir = "";
+if (isset($_FILES['product_image']) ) {
+    
+    $image_dir = "assets/images/products/";
+    $imagename =  imageUpload('product_image',$image_dir);
+}
+else{
+    $imagename = post('image');
 }
 
 
-// insert data to dataabse 
+// insert to database with image path 
 
-foreach ($data as $key => $value) {
-    $db->query("INSERT INTO makeup_products (product_id,product_name,product_description,product_image,
-    product_category,brand,product_price,product_quantity,product_quantity
-    ) VALUES (
-        :product_id,
-        :product_name,
-        :product_description,
-        :product_image,
-        :product_category,
-        :brand,
-        :product_price,
-        :product_quantity,
-    )", [
-        'product_id' => $value['product_id'],
-        'product_name' => $value['product_name'],
-        'product_description' => $value['product_description'],
-        'product_image' => $imagesdir[$key],
-        'product_category' => $value['product_category'],
-        'brand' => $value['brand'],
-        'product_price' => $value['product_price'],
-        'product_quantity' => $value['product_quantity'],
-    ])->statement->execute();
-}
-
-
-
-// echo $image_dir.$imagename;
+$db->query("INSERT INTO makeup_products (product_id,product_name,product_image,product_category,brand,product_price,product_quantity) VALUES (
+    :product_id,
+    :product_name,
+    :product_image,
+    :product_category,
+    :brand,
+    :product_price,
+    :product_quantity
+)",[
+    'product_id' => post('product_id'),
+    'product_name' => post('product_name'),
+    'product_image' => $image_dir.$imagename,
+    'product_category' => post('product_category'),
+    'brand' => post('brand'),
+    'product_price' => post('product_price'),
+    'product_quantity' => post('product_quantity')
+]);
