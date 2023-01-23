4a7acd2a4792523dd4748ed4592ba8583a91bd20
elhajuojy <souirimehdi311@gmail.com>
2023-01-23 15:40:22 +0100

    Always Escape Untrusted value

--- website/views/components/BaseCard.php
+++ website/views/components/BaseCard.php
@@ -1,15 +1,15 @@
 <sl-card class="card-overview">
-    <img slot="image" src="<?=$img?>" alt="<?=$imgAlt?>" />
+    <img slot="image" src="<?=htmlspecialchars($img)?>" alt="<?=htmlspecialchars($imgAlt)?>" />
 
-    <strong><?=$title?></strong><br />
+    <strong><?=htmlspecialchars($title)?></strong><br />
     <?=$content?><br />
-    <small><?=$smallDescription?></small>
+    <small><?=htmlspecialchars($smallDescription)?></small>
 
     <div slot="footer">
-        <a href="/productDetail?id=<?=$id?>">
-            <button class="text-blue-600" variant="secondry" pill><?=$btnText?></button>
+        <a href="/productDetail?id=<?=htmlspecialchars($id)?>">
+            <button class="text-blue-600" variant="secondry" pill><?=htmlspecialchars($btnText)?></button>
         </a>
         <!-- <sl-rating></sl-rating> -->
-        <p>$<span><?=$price?></span></p>
+        <p>$<span><?=htmlspecialchars($price)?></span></p>
     </div>
 </sl-card>
