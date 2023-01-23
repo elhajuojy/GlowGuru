8c08ae1975b01fd9221f4a3dfabd7e412a636692
elhajuojy <souirimehdi311@gmail.com>
2023-01-23 11:35:29 +0100

    update routes access slach problem on macOs

--- website/routes.php
+++ website/routes.php
@@ -1,24 +1,26 @@
 <?php 
 
 
-$router->get('/', 'Controllers\HomeController.php');
+$router->get('/', 'controllers/HomeController.php');
+$router->get('/home', 'controllers/HomeController.php');
+
 // $router->get('/product', 'Controllers\ProductsController.php');
 
 
-$router->get('/admin/login', 'Controllers\Admin\Login.php');
-$router->post('/admin/login', 'Controllers\Admin\Login.php');
-$router->get('/admin/logout', 'Controllers\Admin\Logout.php');
+$router->get('/admin/login', 'controllers/Admin/Login.php');
+$router->post('/admin/login', 'controllers/Admin/Login.php');
+$router->get('/admin/logout', 'controllers/Admin/Logout.php');
 
 
 
-$router->get('/admin/dashboard', 'Controllers\Admin\Dashboard.php');
+$router->get('/admin/dashboard', 'Controllers/Admin/Dashboard.php');
 
-$router->get('/admin/dashboard/products', 'Controllers\Admin\Dashboard.php');
+$router->get('/admin/dashboard/products', 'Controllers/Admin/Dashboard.php');
 
-$router->get('/product', 'Controllers\Admin\product\show.php');
-$router->post('/product', 'Controllers\Admin\product\create.php');
-$router->delete('/product', 'Controllers\Admin\product\destory.php');
-$router->update('/product', 'Controllers\Admin\product\update.php');
+$router->get('/product', 'Controllers/Admin/product/show.php');
+$router->post('/product', 'Controllers/Admin/product/create.php');
+$router->delete('/product', 'Controllers/Admin/product/destory.php');
+$router->update('/product', 'Controllers/Admin/product/update.php');
 
 
-$router->get('/productDetail', 'Controllers\product\showDetail.php');
+$router->get('/productDetail', 'Controllers/product/showDetail.php');
