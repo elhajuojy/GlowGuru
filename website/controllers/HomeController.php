265d101ec286a80aeb40ffa5056196ee3ecdfced
elhajuojy <souirimehdi311@gmail.com>
2023-01-23 16:50:41 +0100

    add best Products

--- website/controllers/HomeController.php
+++ website/controllers/HomeController.php
@@ -6,10 +6,12 @@ use core\Database;
 $db = App::resolve(Database::class);
 
 $products  = $db->query('SELECT * FROM makeup_products limit 6')->statement->fetchAll();
+$bestSellingProducts  = $db->query('SELECT * FROM makeup_products limit 6 offset 6')->statement->fetchAll();
 
 
 
 view('home.view', [
     'title' => 'Home',
-    'products' => $products
+    'products' => $products,
+    'bestSellingProducts' => $bestSellingProducts
 ]);
